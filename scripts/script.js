4c4bad3de15bf9b92a09d9bbeff4bf1233e55cf1
AnasFaaiz <2310030087@klh.edu.in>
2024-11-15 20:00:42 +0530

    Added flipbook & more

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+
+function goToPage() {
+    // Specify the URL of the new HTML page
+    window.location.href = "pages/Flipbook-view.html";
+}
+function goToWeb() {
+    // Specify the URL of the new HTML page
+    window.location.href = "../index.html";
+}
